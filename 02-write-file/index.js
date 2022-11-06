5a2b38cdd3937e12d4f35c55b0b8033a747c4e5b
Earn1ll <coldsteelblr@gmail.com>
2022-11-06 20:31:18 +0300

    feat: complete task 02

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+const { stdin, stdout } = process;
+const fs = require("fs");
+const path = require("path");
+const { exit } = require("process");
+const newFile = path.join(__dirname, "text.txt");
+fs.writeFile(newFile, "", (err) => {
+  if (err) throw err;
+  console.log(
+    "HELLO!, введите текст,который будет записан в text.txt:"
+  );
+});
+stdin.on("data", (data) => {
+  if (data.toString().trim() === "exit") {
+    exitFromStdin();
+  } else {
+    fs.appendFile(newFile, data.toString().replace('\r\n', ''), (err) => {
+        if (err) { console.log(err) };
+      });
+  }
+});
+process.on("SIGINT", exitFromStdin);
+function exitFromStdin() {
+  process.on("exit", () =>
+    console.log("GOODBYE, my darling friend!")
+  );
+  process.exit();
+}
